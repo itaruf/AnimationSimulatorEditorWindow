fb8d76b33562e90467e1688a6dca252aa1640686
itaruf <imane93100@hotmail.fr>
2022-06-01 22:54:38 +0200

    Update

--- Assets/Editor/AnimationSimulatorWindow.cs
+++ Assets/Editor/AnimationSimulatorWindow.cs
@@ -7,17 +7,25 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 using System.Linq;
 
+#if UNITY_EDITOR
+[ExecuteInEditMode]
 [CustomEditor(typeof(IdleChanger))]
 public class AnimationSimulatorWindow : EditorWindow
 {
-    public List<Animator> _animators;
+    public Animator[] _animators;
     Vector2 scrollPos = Vector2.zero;
     string t = "This is a string inside a Scroll view!";
     static AnimationSimulatorWindow window;
+    bool isPlaying = false;
 
+    // Subscribing to events
+    static AnimationSimulatorWindow()
+    {
+        UnityEditor.SceneManagement.EditorSceneManager.sceneClosing += SceneClosing;
+    }
     void Start()
     {
-
+        Debug.Log("TEST");
     }
 
     void Update()
@@ -28,19 +36,26 @@ public class AnimationSimulatorWindow : EditorWindow
     [MenuItem("Window/Animator Simulator")]
     public static void ShowWindow()
     {
+        UnityEditor.SceneManagement.EditorSceneManager.sceneClosing += SceneClosing;
+
         window = (AnimationSimulatorWindow)GetWindowWithRect(typeof(AnimationSimulatorWindow), new Rect(0, 0, 300, 300));
         window.Show();
+    }
+
+    void Init()
+    {
+
     }
 
     void OnGUI()
     {
         // Find all animators in the scene
-        _animators = FindObjectsOfType<Animator>().ToList();
+        _animators = GetAnimatorsInScene();
 
         EditorGUILayout.BeginVertical();
         scrollPos = EditorGUILayout.BeginScrollView(scrollPos, false, false);
 
-        GUILayout.Label($"Animators : {_animators.Count}", EditorStyles.boldLabel);
+        GUILayout.Label($"Animators : {_animators.Length}", EditorStyles.boldLabel);
 
         // List all animators in the scene
         ListAnimators();
@@ -59,19 +74,56 @@ public class AnimationSimulatorWindow : EditorWindow
             if (GUILayout.Button(a.name))
                 Selection.activeGameObject = a.gameObject;
         }
-
     }
+
     void ListAnimations()
     {
         if (Selection.activeGameObject)
         {
             if (Selection.activeGameObject.TryGetComponent(out Animator animator))
             {
-                foreach (var animation in animator.runtimeAnimatorController.animationClips)
+                AnimationClip[] clips = animator.runtimeAnimatorController.animationClips;
+                foreach (var a in clips)
                 {
-                    GUILayout.Label(animation.name, EditorStyles.boldLabel);
+                    if (GUILayout.Button(a.name))
+                    {
+                        isPlaying = false;
+                        PlayClip(a, animator);
+                    }
                 }
             }
         }
     }
+
+    void PlayClip(AnimationClip clip, Animator animator)
+    {
+        isPlaying = true;
+
+        clip.SampleAnimation(animator.gameObject, Time.deltaTime);
+        animator.Update(Time.deltaTime);
+    }
+
+    static Animator[] GetAnimatorsInScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (!scene.IsValid()) return null;
+
+        List<Animator> AnimatorList = new List<Animator>();
+
+        GameObject[] rootGameObjects = scene.GetRootGameObjects();
+        foreach (GameObject rootGameObject in rootGameObjects)
+        {
+            AnimatorList.AddRange(rootGameObject.GetComponentsInChildren<Animator>(true));
+        }
+
+        return AnimatorList.ToArray();
+    }
+
+    static void SceneClosing(UnityEngine.SceneManagement.Scene scene, bool removingScene)
+    {
+        Debug.Log("SceneClosing");
+    }
 }
+
+#endif
